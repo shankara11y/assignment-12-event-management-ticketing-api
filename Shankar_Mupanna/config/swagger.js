eb4cf8592eccc0f89b2c0181a66c8c8716963f95
Shankar Mupanna <smupanna@gmail.com>
2026-09-25 21:05:42 +0530

    some changes

--- Shankar_Mupanna/config/swagger.js
+++ Shankar_Mupanna/config/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: `http://localhost:${process.env.PORT || 5003}`,
         description: 'Development Server'
       }
     ],
